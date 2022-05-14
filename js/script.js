f2f10937a456e9cbde731059b9629dee8ae2c4f6
AlexKrynytskyi <alex.krynytskyi@gmail.com>
2022-05-14 13:00:42 +0300

    finish

--- js/script.js
+++ js/script.js
@@ -1,22 +1,5 @@
 //=============== home slider
 
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 1,
-  spaceBetween: 0,
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  autoplay: {
-    delay: 7000,
-    disableOnInteraction: false,
-  },
-});
 
 // ============ show mobile=menu
 
